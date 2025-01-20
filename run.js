6ea5f1395d37269ee21b778d3932fc8677cf16e1
Ramana Kumar <ramana@member.fsf.org>
2025-01-20 07:54:47 +0000

    Fix which exceptional proposal to discard

--- run.js
+++ run.js
@@ -153,7 +153,7 @@ async function getPayload(slotNumber, relayName, relayApiUrl) {
   if (relayName == 'Flashbots' && slotNumber == 10599355 &&
       payloads.length == 2 && payloads[0].value == '20541292660255419') {
     console.warn(`Special case: discarding extra ${relayName} payloads for slot ${slotNumber}`)
-    payloads.pop()
+    payloads.shift()
   }
   if (!(payloads instanceof Array && payloads.length <= 1)) {
     console.warn(`Unexpected result for ${slotNumber} payload from ${relayName}: ${JSON.stringify(payloads)}`)
